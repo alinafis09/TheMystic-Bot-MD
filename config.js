c077a3b974deb273819719052ff8a6d10ee7e1ff
alinafis09 <alinafis20055@gmail.com>
2025-07-08 19:19:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ import {fileURLToPath} from 'url';
 import fs from 'fs'; 
 import moment from 'moment-timezone';
 
-global.botnumber = ""
+global.botnumber = "212624052666"
 global.confirmCode = ""
 global.authFile = `MysticSession`;
 
